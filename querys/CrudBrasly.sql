4a3ce84f2e7e0b7f959f39097602aa43c0d08dd2
braslyvm <braslymorales@gmail.com>
2025-11-01 22:27:29 -0600

    Crud de la tabla rol

--- querys/CrudBrasly.sql
+++ querys/CrudBrasly.sql
@@ -12,3 +12,50 @@ begin
 end;
 $$;
 
+-- delete Rol
+create procedure delete_rol(
+    U_id INTEGER
+)
+language plpgsql
+as $$
+begin
+    delete from rol Where id = U_id;
+end;
+$$;
+
+
+-- update Rol
+create or replace procedure update_rol(
+    p_id integer,
+    p_nombre varchar(50) default null,
+    p_descripcion text default null
+)
+language plpgsql
+as $$
+begin
+    update rol
+    set 
+        nombre = coalesce(nullif(p_nombre, ''), nombre),
+        descripcion = coalesce(nullif(p_descripcion, ''), descripcion)
+    where id = p_id;
+end;
+$$;
+
+-- Select Rol
+create function select_rol()
+returns table (
+    id integer,
+    nombre varchar(50),
+    descripcion text
+)
+language plpgsql
+as $$
+begin
+    return query
+    select id, nombre, descripcion
+    from rol;
+end;
+$$;
+
+
+
